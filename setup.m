ad3a66f86606deae1cdc1ad25960a2c91e901da9
Katerina <Katerina@localhost>
2013-04-08 01:02:48 +0400

    Comments have been added to setup file.

--- setup.m
+++ setup.m
@@ -1,30 +1,49 @@
 function setup()
-    
+    % Install and uninstall singleton package
+    %
+    % Usage:
+    %   setup()
+    %
+    % Input commands:
+    %   - install   : Add package directories to MATLAB search paths
+    %   - uninstall : Remove package directories from MATLAB search paths
+    %
+
+    % ---------------------------------------------------------------------
+    % Package   : singleton
+    % Version   : 1.1
+    % Author    : Evgeny Prilepin <esp.home@gmail.com>
+    % Created   : 10.01.2013
+    % Updated   : 05.04.2013
+    %
+    % Copyright : (C) 2013 by Evgeny Prilepin
+    % ---------------------------------------------------------------------
+
     action = input('Setup: [Install]/Uninstall:', 's');
-    
+
     if isempty(action)
         action = 'i';
     end
-    
+
     action = validatestring(action, {'install', 'uninstall'});
-    
+
     d = fileparts(mfilename('fullpath'));
-    
+
     p = {
         d
         fullfile(d, 'examples')
         fullfile(d, 'tests')
         };
-    
+
     switch action
         case 'install'
             addpath(p{:});
             disp('install done');
-            
+
         case 'uninstall'
             cellfun(@(x) rmpath(x), p);
             disp('uninstall done');
     end
-    
+
 	savepath();
 end
